64b818d1d11e7f56db90a541ac27236a5a10281f
Sumit Satre <sumitsatre03@gmail.com>
2023-07-03 09:11:05 +0530

    Push

--- Node JS/http_module2.js
+++ Node JS/http_module2.js
@@ -4,8 +4,11 @@ const http = require("http");
 const { json } = require("stream/consumers");
 const data = require("./data_for_redux.js");
 
-http.createServer((req , res)=>{
-    res.write(JSON.stringify(data));
-    res.end();
-}).listen(5000);
+let host = '127.0.0.1';
+
+http.createServer((req, res) => {
+  res.write(JSON.stringify(data));
+  res.end();
+}).listen(5000, host);
+
 
